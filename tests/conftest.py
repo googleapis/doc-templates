16be9a09b23c66989584d73b310294d1c58f79c1
Tyler Bui-Palsulich <tbp@google.com>
2020-09-18 11:07:32 -0400

    tests: add golden test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,22 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+import pytest
 
-# Disable buffering, so that the logs stream through.
-export PYTHONUNBUFFERED=1
 
-# Add the path where pip installs commands to PATH.
-export PATH=$PATH:${HOME}/.local/bin
+def pytest_addoption(parser):
+    parser.addoption("--update-goldens", action="store", default=False)
 
-python3 -m pip install -r requirements.txt
-python3 -m pip install flake8 black pytest pytest-cov
 
-black --check tests
-flake8 tests
-
-if [ -n "$UPDATE_GOLDENS" ]; then
-    pytest --update-goldens tests
-else
-    pytest tests
-fi
+@pytest.fixture
+def update_goldens(request):
+    return request.config.getoption("--update-goldens")
---
+++ tests/test_goldens.py
@@ -0,0 +1,66 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+import filecmp
+import os
+import shutil
+
+from docuploader import shell
+
+import pytest
+
+
+def test_goldens(update_goldens):
+    test_dir = "python-small"
+    build_dir = Path("testdata") / test_dir
+    golden_dir = Path("testdata/goldens") / test_dir
+    out_dir = build_dir / "site/api"
+    # Generate!
+    try:
+        shell.run(
+            [
+                "docfx",
+                "build",
+                "-t",
+                "default,../../third_party/docfx/templates/devsite",
+            ],
+            cwd=build_dir,
+            hide_output=False,
+        )
+    except Exception as e:
+        pytest.fail(f"build raised an exception: {e}")
+
+    if update_goldens:
+        shutil.rmtree(golden_dir, ignore_errors=True)
+        shutil.copytree(out_dir, golden_dir, dirs_exist_ok=True)
+        pytest.skip(
+            "Updated goldens! Re-run the test without the --update-goldens flag."
+        )
+
+    got_files = [os.path.relpath(f, out_dir) for f in out_dir.rglob("*")]
+    golden_files = [os.path.relpath(f, golden_dir) for f in golden_dir.rglob("*")]
+
+    assert len(got_files) == len(
+        golden_files
+    ), f"got {len(got_files)} files, want {len(golden_files)}"
+
+    (eq, neq, other) = filecmp.cmpfiles(out_dir, golden_dir, got_files, shallow=False)
+    neq = [(out_dir / f).as_posix() for f in neq]
+    other = [(out_dir / f).as_posix() for f in other]
+
+    if other:
+        pytest.fail(f"found unknown files (should never happen): {other}")
+    if neq:
+        pytest.fail(f"got files that don't match goldens: {neq}")
