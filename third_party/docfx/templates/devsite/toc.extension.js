9d738fb58c02587413d1f5d1958fa6ad21019248
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-01-14 18:05:50 -0500

    cleanup: rename toc.html to toc.yaml (#111)

--- third_party/docfx/templates/devsite/toc.extension.js
+++ third_party/docfx/templates/devsite/toc.extension.js
@@ -1,14 +1,14 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 /**
- * This method will be called at the start of exports.transform in toc.html.js
+ * This method will be called at the start of exports.transform in toc.yaml.js
  */
 exports.preTransform = function (model) {
   return model;
 }
 
 /**
- * This method will be called at the end of exports.transform in toc.html.js
+ * This method will be called at the end of exports.transform in toc.yaml.js
  */
 exports.postTransform = function (model) {
   return model;
