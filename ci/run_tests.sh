74871c2077861edbfbe4e7b414b6fd50145118a0
Jon Skeet <jonskeet@google.com>
2021-02-12 09:43:52 +0000

    fix: Run docfx from a local copy when testing
    This is a horrible hack that needs to be addressed properly.
    In order to run plugins, docfx creates a subdirectory under the
    docfx executable's directory. That fails when docfx is in a
    read-only directory. This is really a design issue in docfx, but I
    don't expect that to be fixed imminently.
    
    In order to run plugins in the docfx pipeline, we'll either need to
    do something like this hack in doc-pipeline as well, or change the
    Dockerfile to make /opt/docfx writable.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -21,6 +21,18 @@ export PYTHONUNBUFFERED=1
 # Add the path where pip installs commands to PATH.
 export PATH=$PATH:${HOME}/.local/bin
 
+# Copy docfx to a local directory, so that we'll be able
+# to write to a plugins directory. In the future we may
+# want to change the Dockerfile instead.
+cp -r /opt/docfx ${HOME}/localdocfx
+chmod a+w ${HOME}/localdocfx
+export PATH=${HOME}/localdocfx:$PATH
+
+# The docfx script just runs Mono. We need to make it
+# run the right docfx.exe...
+echo '#!/bin/bash' > ${HOME}/localdocfx/docfx
+echo "exec mono ${HOME}/localdocfx/docfx.exe \$@" >> ${HOME}/localdocfx/docfx
+
 python3 -m pip install -r requirements.txt
 python3 -m pip install flake8 black pytest pytest-cov
 
