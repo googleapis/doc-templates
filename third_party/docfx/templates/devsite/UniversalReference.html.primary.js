1746065af7d6cdf77669373bea907ab45d7b7231
dansaadati <dsaadati@google.com>
2022-04-01 10:50:24 -0700

    feat(ruby): add friendly api name to page title

--- third_party/docfx/templates/devsite/UniversalReference.html.primary.js
+++ third_party/docfx/templates/devsite/UniversalReference.html.primary.js
@@ -13,6 +13,7 @@ exports.transform = function (model) {
   }
 
   model._disableToc = model._disableToc || !model._tocPath || (model._navPath === model._tocPath);
+  model._displayFriendlyApiName = model._displayFriendlyApiName && model.friendlyApiName;
 
   if (extension && extension.postTransform) {
     model = extension.postTransform(model);
